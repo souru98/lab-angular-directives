b9bf281650752d4c57ea5497de9a804f32ca0a52
souru98 <kulkarnisourabh1998@gmail.com>
2020-04-04 20:00:36 +0530

    done

---
+++ directive-ngif-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directive-ngif-app';
+}
+
+// export class NgIfSimple {
+//   show: boolean = true;
+// }
